0aab16f35adb20a090f5db1f09fd7e6cb9e6cdbe
luis.nunes <luisj2felipe09@gmail.com>
2023-09-02 14:02:31 -0300

    :goal_net: Catch global errors

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -9,7 +9,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string(),
+    password: z.string().min(6),
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
@@ -20,8 +20,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    if (error instanceof UserAlreadyExists)
+    if (error instanceof UserAlreadyExists) {
       return reply.status(409).send({ message: error.message })
+    }
   }
 
   return reply.status(201).send()
