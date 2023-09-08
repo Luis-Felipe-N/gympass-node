76c5bdbc478743cf5840566aea505b7a54d32945
luis.nunes <luisj2felipe09@gmail.com>
2023-09-08 19:02:48 -0300

    :white_check_mark: Register e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be to able register', async () => {
+    const response = await request(app.server).post('/users').send({
+      email: 'luiss@gmail.com',
+      name: 'Luis Felipe',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
